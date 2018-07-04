12aec74f3062685e2209c28705aa62248adc54ca
BogdanovL <39818939+BogdanovL@users.noreply.github.com>
2018-07-04 10:38:13 -0700

    Fix logic in manageHeartBeat

--- Executive/Executive.c
+++ Executive/Executive.c
@@ -27,7 +27,7 @@ static void manageHeartBeat(unsigned frameCount)
 {
 	static const unsigned HEART_BEAT_RATE_MS = 500;
 	static bool ledCmd = false;
-	if ((frameCount % HEART_BEAT_RATE_MS/MS_PER_FRAME) == 0)
+	if (frameCount % (HEART_BEAT_RATE_MS/MS_PER_FRAME) == 0)
 	{
 		digitalWrite(HEARTBEAT_LED, ledCmd);
 		ledCmd = !ledCmd;
