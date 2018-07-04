9a437a5cd03b8ab4e2919564f29c154c2074fcd2
BogdanovL <39818939+BogdanovL@users.noreply.github.com>
2018-07-04 11:07:35 -0700

    Added comment discussing an edge case

--- Executive/Executive.c
+++ Executive/Executive.c
@@ -28,6 +28,7 @@ static void manageHeartBeat(unsigned frameCount)
 	static const unsigned HEART_BEAT_RATE_MS = 500;
 	static bool ledCmd = false;
 
+	// During overflow to zero this will blink twice
 	if (frameCount % (HEART_BEAT_RATE_MS / MS_PER_FRAME) == 0)
 	{
 		digitalWrite(HEARTBEAT_LED, ledCmd);
