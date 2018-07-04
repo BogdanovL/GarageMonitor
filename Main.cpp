75f730ce4446b9fb4aff300e79512aeb06fdbd84
BogdanovL <39818939+BogdanovL@users.noreply.github.com>
2018-07-04 10:02:02 -0700

    Edits to main
    Some comments, updated frame delay, some stylistic fixes.

--- Main.cpp
+++ Main.cpp
@@ -5,16 +5,13 @@
 #include "Executive/Executive.h"
 
 
-
-
-
-
 // Arduino setup entry point
 extern void setup() {
 	Serial.begin(9600);
 	Serial.setDebugOutput(true);
 	ExecInit();
 
+	return;
 
 }
 
@@ -24,30 +21,35 @@ extern void loop()
 	// Count frames away. Basic unit of measurement.
 	static unsigned overFlowingFrameCounter = 0;
 
-	unsigned delayTimeTillNextFrame = MS_PER_FRAME;
-	ExecOutput_structType execOut;
+	unsigned delayTimeTillNextFrame;
 	overFlowingFrameCounter++;
 
-
 	/* --- Start Foreground Frame ---*/
-	// Run our main tasks
 	ManageExec(overFlowingFrameCounter);
 	/* --- End Foreground Frame ---*/
 
+	// Get some info about the frame
+	ExecOutput_structType execOut;
 	GetExecData(&execOut);
+
+	// Calculate new delay time
+	delayTimeTillNextFrame = MS_PER_FRAME - execOut.lastFrameTimeMS;
 
 	// If we over-ran
 	if (execOut.lastFrameTimeMS > MS_PER_FRAME)
 	{
-		// We'll accept the overrun and restart the frame ASAP
-		// We would fault here, but MQTT libraries are non deterministic
+		// We could fault here, but MQTT libraries are non deterministic
 		// and I did not feel like re-writing them. No real 'real time'
-		// here
+		// requirements here, so let's just move on with our lives
+
+		// We'll accept the overrun and restart the frame ASAP
 		delayTimeTillNextFrame = 0;
 
 	}
 
 	// Mostly start tasks every MS_PER_FRAME
 	delay(delayTimeTillNextFrame);
+
+	return;
 
 }
